b8e50391dc21b25891a4c4442e261938f143780e
Kentaro UONO <info@kuono.net>
2022-11-21 01:07:19 +0900

    0.16.1 Linux での STM 関連エラーに対応

--- app/Main.hs
+++ app/Main.hs
@@ -10,20 +10,22 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 --
+import qualified ECU
+import qualified UI
 import Lib
     ( currentTime,
       defaultUSBPathMac,
       defaultUSBPathRaspberryPi,
       frameTitle,
       localTimetoString,
+      logFileName,
       AfterAction(Quit, Shutdown, Restart),
       OSEnv(RaspberryPiOS, MacOS) )
+-- * framework 
 import Brick ( customMain )
 import qualified Brick.BChan as BC
-import qualified ECU
-import UI
-    ( Status(after, cchan), initialState, ecuMonitor, frametoTable ) 
-
+import qualified Graphics.Vty as V
+-- * general purpose library
 import Control.Concurrent ( forkIO )
 import Control.Concurrent.STM.TChan ( TChan, newTChan, writeTChan, readTChan )
 import Control.Exception as Ex ( catch, SomeException )
@@ -32,24 +34,61 @@ import Control.Monad.STM ( atomically )
 import Data.Fixed ( showFixed )
 import Data.Time
     ( LocalTime(LocalTime), TimeOfDay(todSec, todHour, todMin) )
-import System.Environment ( getArgs, getEnv ) --,getEnvironment)
-import System.IO
-    ( Handle,
-      hClose,
-      hFlush,
-      hPutStr,
-      hPutStrLn,
-      withFile,
-      IOMode(WriteMode) )
+import qualified System.Environment as Sys ( getArgs, getEnv ) --,getEnvironment)
+import System.IO ( Handle,IOMode(WriteMode),
+      hClose, hFlush, hPutStr, hPutStrLn, withFile )
 import System.Process ( system ) -- for auto exiting on Raspberry Pi 
-import Text.Printf ( printf ) 
-
-import qualified Graphics.Vty as V
+import Text.Printf (printf)
+import Control.Exception.Base (throwIO)
+import Control.Exception (AsyncException(ThreadKilled))
+import Control.Concurrent (throwTo)
 --
--- 
+-- | Main action 主関数
+main :: IO ()
+main = do
+    args <- Sys.getArgs
+    envs <- Sys.getEnv "HOME"
+    let buildVty = V.mkVty V.defaultConfig
+        (os, path) = case (null args,envs == "/Users/kuono") of
+            (True,True) -> ( Lib.MacOS         , Lib.defaultUSBPathMac         )
+            (True,_   ) -> ( Lib.RaspberryPiOS , Lib.defaultUSBPathRaspberryPi )
+            _           -> if length args == 1 
+                               then ( os , head args )
+                               else error "error: exactly one arguments needed."
+    iniVty  <- buildVty
+    evntCh  <- BC.newBChan 10      :: IO (BC.BChan ECU.Event    ) -- ^ brick で使うイベントチャネルの初期化      
+    ucmdCh  <- atomically newTChan :: IO (TChan    ECU.UCommand ) -- ^ ECUドライバへの司令チャネル。TQueue化予定
+    logCh   <- atomically newTChan :: IO (TChan    ECU.Event    ) -- ^ 
+    --
+    iStatus <- UI.initialState
+    -- | ここから並列処理が始まるが，プロセスの処理が原始的すぎ。
+    --   現時点では ecu はごく少数かつ増加しないプロセスによる素朴な並列処理アーキテクチャを
+    --   採用しているため大きな問題は起こさないが，より多くのデバイスの追加などに対応できない。
+    --   階層化eDSLを使った一般化をすべきだろう。
+    logT <- forkIO $ runlog logCh
+    _ <- forkIO $ forever $ ECU.run (path,evntCh,ucmdCh,logCh) -- ^ デバイスドライバの起動 
+
+    finalState <- Brick.customMain iniVty buildVty (Just evntCh) UI.ecuMonitor iStatus
+
+    let cmdchan = UI.cchan finalState
+    atomically $ writeTChan cmdchan ECU.Disconnect   -- これで各スレッドは落としている 
+    throwTo logT ThreadKilled
+    Prelude.putStrLn "Thank you for using Mini ECU Monitor. See you again!"
+    -- killThread ecuT -- いきなり kill すると支障が出るか，要調査
+    -- killThread logT -- = throw ecuT ThreadKilled <- hCloseしている
+    _ <- system $ if os /= Lib.RaspberryPiOS then
+                    "echo \"\""  -- ":" is a command do nothing on bash
+                  else
+                    case UI.after finalState of
+                      Lib.Shutdown ->  "sudo shutdown -h now" 
+                      Lib.Restart  ->  "sudo shutdown -r now"
+                      Lib.Quit     ->  "echo \"\""  -- ":" is a command do nothing on bash
+    return ()
+--
+-- | logger 
 runlog :: TChan ECU.Event -> IO ()
 runlog ech = forever $ do
-  l <- logFileName :: IO FilePath
+  l <- Lib.logFileName :: IO FilePath
   withFile l WriteMode $
     \h -> do
       hPutStrLn h  $ "Date,Time," ++ frameTitle
@@ -62,7 +101,7 @@ runlog ech = forever $ do
               let j  = localTimetoString t
               hPutStr h $ j ++ ","
               hPutStr h $ case e of 
-                ECU.Tick r         -> frametoTable $ ECU.parse r
+                ECU.Tick r         -> ECU.frametoTable $ ECU.parse r
                 ECU.GotIACPos p    -> " Got IAC Pos    : " ++ show p
                 ECU.PortNotFound f -> " Port Not Found : " ++ f
                 ECU.Connected m    -> " Connected      : " ++ show (ECU.model m)
@@ -92,42 +131,3 @@ logFileName = do
                                     else '0':showFixed False byo
               in printf "ECULog%10s_%02d.%02d.%2s.csv" hi ji hun byo' -- ex. ECULog2018-10-15_17.27.26.csv
               --  ./log/ECU...としていたが，ディレクトリが存在していないとランタイムエラーを起こすので変更
--- | Main action 主関数
-main :: IO ()
-main = do
-    args <- System.Environment.getArgs
-    envs <- System.Environment.getEnv "HOME"
-    let buildVty = V.mkVty V.defaultConfig
-        (os, path,intestmode) = case (null args,envs == "/Users/kuono") of
-            (True,True) -> ( MacOS         , defaultUSBPathMac         , False )
-            (True,_   ) -> ( RaspberryPiOS , defaultUSBPathRaspberryPi , False )
-            _           -> if length args == 1 
-                               then ( os , head args , True )
-                               else error "error: exactly one arguments needed."
-    iniVty  <- buildVty    
-    evntCh  <- BC.newBChan 10      :: IO (BC.BChan ECU.Event    )            
-    ucmdCh  <- atomically newTChan :: IO (TChan    ECU.UCommand ) 
-    logdCh  <- atomically newTChan :: IO (TChan    ECU.Event    )
-
-    iStatus <- initialState
-    -- | ここの処理が原始的すぎ。
-    _ <- forkIO $ runlog logdCh
-    _ <- forkIO $ forever $ ECU.run (path,evntCh,ucmdCh,logdCh) 
-
-    finalState <- Brick.customMain iniVty buildVty (Just evntCh) ecuMonitor iStatus
-
-    let cmdchan = cchan finalState
-    _ <- atomically $ writeTChan cmdchan ECU.Disconnect   -- これで各スレッドは落としている 
-
-    Prelude.putStrLn "Thank you for using Mini ECU Monitor. See you again!"
-    -- killThread ecuT -- いきなり kill すると支障が出るか，要調査
-    -- killThread logT -- = throw ecuT ThreadKilled <- hCloseしている
-    _ <- system $ if os /= RaspberryPiOS then
-                    "echo \"\""  -- ":" is a command do nothing on bash
-                  else
-                    case after finalState of
-                      Shutdown ->  "sudo shutdown -h now" 
-                      Restart  ->  "sudo shutdown -r now"
-                      Quit     ->  "echo \"\""  -- ":" is a command do nothing on bash
-    return ()
-
